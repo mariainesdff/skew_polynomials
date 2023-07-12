009b941048586e324ec78cedbf1e3ac71d8896bd
Xavier Généreux <xaviergenereux@hotmail.com>
2023-07-12 15:46:45 +0200

    Update and rename SkewMul to SkewMul.lean
    Added power to hom

--- SkewMul.lean
+++ SkewMul.lean
@@ -8,7 +8,7 @@ variable [Semiring k] [Add G] (φ : k →+* k) {f g : AddMonoidAlgebra k G}
 namespace SkewMul
 
 instance hasMul' (φ : k →+* k): Mul (AddMonoidAlgebra k G) :=
-  ⟨fun f g => f.sum fun a₁ b₁ => g.sum fun a₂ b₂ => single (a₁ + a₂) (b₁ * (φ b₂))⟩
+  ⟨fun f g => f.sum fun a₁ b₁ => g.sum fun a₂ b₂ => single (a₁ + a₂) (b₁ * (φ^[a₁] b₂))⟩
 
 
 --set_option quotPrecheck false
