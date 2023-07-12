8b7e6fbb570ebe907e7da571b5f0a646602029c6
Carlos Caralps <78864605+carloscaralps@users.noreply.github.com>
2023-07-12 11:06:24 +0200

    Initial definition

--- SkewPolynomials.lean
+++ SkewPolynomials.lean
@@ -1,1 +1,26 @@
 import Mathlib.Algebra.MonoidAlgebra.Basic
+
+noncomputable section
+
+structure SkewPolynomial (R : Type _) (φ : R → R) [Semiring R] where ofFinsupp ::
+  toFinsupp : AddMonoidAlgebra R ℕ
+#align polynomial SkewPolynomial
+#align polynomial.of_finsupp SkewPolynomial.ofFinsupp
+#align polynomial.to_finsupp SkewPolynomial.toFinsupp
+
+variable 
+
+-- mathport name: polynomial
+scoped[SkewPolynomial] notation:9000 R["X",φ] => SkewPolynomial R φ
+
+open AddMonoidAlgebra
+open Finsupp hiding single
+open Function hiding Commute
+
+open BigOperators SkewPolynomial
+
+namespace SkewPolynomial
+
+universe u
+
+variable {R : Type u} {a b : R} {m n : ℕ}
