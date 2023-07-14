a248788bc19114a807df436aa32728fcdb000f35
Xavier Généreux <xaviergenereux@hotmail.com>
2023-07-14 12:11:46 +0200

    Update Drinfeld_Module.lean
    Added proof for Frob

--- Drinfeld_Module.lean
+++ Drinfeld_Module.lean
@@ -14,9 +14,20 @@ variable (L : Type _) [Field L] [Algebra (𝔽_[p]^n)[X] L]
 noncomputable def Frob : L →+* L where
   toFun     := fun x ↦ x^(p^n)
   map_one'  := by simp only [one_pow]
-  map_mul'  := sorry
-  map_zero' := sorry
-  map_add'  := sorry
+  map_mul'  := fun x y ↦ by
+    simp; rw [mul_pow]
+  map_zero' := by 
+    simp only [ne_eq, gt_iff_lt, zero_pow_eq_zero]
+    refine' pow_pos (Nat.Prime.pos h_prime.1) _
+  map_add'  := fun x y ↦ by
+    simp; 
+    have hFieldAlg: Algebra (𝔽_[p]^n) L := by
+      apply RingHom.toAlgebra ((algebraMap (𝔽_[p]^n)[X] L).comp
+       (algebraMap (𝔽_[p]^n) (𝔽_[p]^n)[X]))
+    have hChar: CharP L p := by
+      apply charP_of_injective_algebraMap' (GaloisField p n) L p
+    rw [add_pow_char_pow L x y]
+
 namespace DrinfeldModule
 
 instance : Algebra (𝔽_[p]^n) L[X; Frob p n L] := sorry
